1b4fc3c19701400fd6f42974f56c1256b271da79
prajjwalkumar17 <prajjwal.kumar@juspay.in>
2023-04-18 07:10:16 +0530

    all files needed configured in lua

---
+++ lua/pk/plugin/rust-tools.lua
@@ -0,0 +1,16 @@
+-- import comment plugin safely
+local setup, rt = pcall(require, "rust-tools")
+if not setup then
+  return
+end
+
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
---
+++ lua/pk/plugin/treesitter.lua
@@ -0,0 +1,46 @@
+-- import nvim-treesitter plugin safely
+local status, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status then
+  return
+end
+
+-- configure treesitter
+treesitter.setup({
+  -- enable syntax highlighting
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting=false,
+  },
+  -- enable indentation
+  indent = { enable = true },
+  -- enable autotagging (w/ nvim-ts-autotag plugin)
+  autotag = { enable = true },
+    rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil,
+  },
+  -- ensure these language parsers are installed
+  ensure_installed = {
+    "json",
+    "javascript",
+    "typescript",
+    "tsx",
+    "yaml",
+    "html",
+    "css",
+    "markdown",
+    "markdown_inline",
+    "svelte",
+    "graphql",
+    "bash",
+    "lua",
+    "vim",
+    "toml",
+    "rust",
+    "dockerfile",
+    "gitignore",
+  },
+  -- auto install above language parsers
+  auto_install = true,
+})
